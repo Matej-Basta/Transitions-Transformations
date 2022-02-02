3b08c1154a643369effe1886e8eebdc85e9f939c
Matej-Basta <71135305+Matej-Basta@users.noreply.github.com>
2022-02-02 16:32:51 +0100

    Finished 2nd exercise

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const box = document.getElementById("exercise_fixed_box");
+const box2 = document.getElementById("exercise_bg_box");
 
 box.addEventListener("click", () => {
   box.classList.toggle("change");
+});
+
+box2.addEventListener("click", () => {
+  box2.classList.toggle("changed");
 });
